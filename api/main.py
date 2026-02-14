0fadc8eb25c3b90857e9c50f7a295b1fffc216c8
roidaradal <roidaradal@gmail.com>
2026-02-14 20:03:18 +0800

    Rearrange functions in main

--- api/main.py
+++ api/main.py
@@ -12,31 +12,6 @@ if not IS_PROD_ENV:
     from dotenv import load_dotenv
     load_dotenv()
 
-app = FastAPI()
-
-@app.get('/')
-async def health_check() -> ActionResult:
-    return ActionResult(success=True, message='OK')
-
-@app.get('/{date_string}')
-async def get_month_data(date_string: str = 'today', devs: str = ''):
-    input_date: date = new_date(date_string)
-    devs_list: list[str] = get_devs(devs)
-    num_devs = len(devs_list)
-    if num_devs == 0:
-        return DataResult(data=None, message='Empty devs list')
-    elif num_devs > DEV_LIMIT:
-        return DataResult(data=None, message=f'Devs list exceeds limit: {DEV_LIMIT}')
-    
-    dev_contribs, err = await get_devs_contribs(devs_list, input_date)
-    if err.has:
-        return DataResult(data=None, message=err.message)
-    
-    return DataResult(data = {
-        'date' : input_date,
-        'contribs': dev_contribs,
-    })
-
 ############################## DATA FUNCTIONS #######################################
 
 okMessage = 'OK'
@@ -153,6 +128,33 @@ async def fetch_dev_contribs(dev: str, input_date: date, client: httpx.AsyncClie
         contribs[str(day)] = Contrib(count = count, level = level)
 
     return dev, contribs, Error()
+
+############################## FAST API ROUTERS #######################################
+
+app = FastAPI()
+
+@app.get('/')
+async def health_check() -> ActionResult:
+    return ActionResult(success=True, message='OK')
+
+@app.get('/{date_string}')
+async def get_month_data(date_string: str = 'today', devs: str = ''):
+    input_date: date = new_date(date_string)
+    devs_list: list[str] = get_devs(devs)
+    num_devs = len(devs_list)
+    if num_devs == 0:
+        return DataResult(data=None, message='Empty devs list')
+    elif num_devs > DEV_LIMIT:
+        return DataResult(data=None, message=f'Devs list exceeds limit: {DEV_LIMIT}')
+    
+    dev_contribs, err = await get_devs_contribs(devs_list, input_date)
+    if err.has:
+        return DataResult(data=None, message=err.message)
+    
+    return DataResult(data = {
+        'date' : input_date,
+        'contribs': dev_contribs,
+    })
 
 '''
 TODO:
