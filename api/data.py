83ce5cafb5dc5bf1e2c170e5816e58ea7f8eb7c8
zeroibot <roidaradal@gmail.com>
2026-08-27 20:55:29 +0800

    Data functions

--- api/data.py
+++ api/data.py
@@ -16,3 +16,19 @@ def get_cors_list() -> list[str]:
     '''Get list of origins allowed for CORS policy'''
     cors = os.getenv('CORS_LIST') or ''
     return [x.strip() for x in cors.split(',')]
+
+def get_devs(devs: str) -> list[str]:
+    '''Get list of devs from the input string'''
+    if devs == '@goodapps':
+        devs = os.getenv('GOODAPPS_DEVS') or ''
+    if devs == '':
+        return []
+    return [x.strip() for x in devs.split(',')]
+
+def get_dev_limit() -> int:
+    ''' Get limit for number of devs'''
+    try:
+        limit = int(os.getenv('DEV_LIMIT') or '9')
+        return max(1, limit) # floor dev limit = 1
+    except:
+        return 9 # default limit
--- old/data.py
+++ old/data.py
@@ -17,20 +17,3 @@ def new_date(date_string: str) -> date:
 def date_format(d: date) -> str:
     '''String representation of date object in standard format'''
     return d.strftime(std_date_format)
-
-def get_devs(devs: str) -> list[str]:
-    '''Get list of devs from the input string'''
-    devs = devs.strip()
-    if devs == '@goodapps':
-        devs = os.getenv('GOODAPPS_DEVS') or ''
-    if devs == '':
-        return []
-    return [x.strip() for x in devs.split(',')]
-
-def get_dev_limit() -> int:
-    '''Get limit for number of devs'''
-    try:
-        limit = int(os.getenv('DEV_LIMIT') or '9')
-        return max(1, limit) # floor dev limit = 1
-    except:
-        return 9 # default limit
