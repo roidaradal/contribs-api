5b7a78d4d75c95a97b2bfd8dd799c89152d65145
zeroibot <roidaradal@gmail.com>
2026-08-27 19:12:19 +0800

    Reset main API

--- api/data.py
+++ api/data.py
@@ -1,49 +1,16 @@
 import os
-from datetime import date
 from pydantic import BaseModel
 from typing import Optional
 
-okMessage = 'OK'
-std_date_format = '%Y-%m-%d'
-    
+OK_MESSAGE = 'OK'
+
 class ActionResult(BaseModel):
     success: bool = True 
-    message: str = okMessage
+    message: str = OK_MESSAGE
 
 class DataResult[T](BaseModel):
-    data: Optional[T] = None
-    message: str = okMessage
-
-def new_date(date_string: str) -> date:
-    '''Parse date_string as date, defaults to date today if invalid date'''
-    today = date.today()
-    if date_string.lower() == 'today':
-        return today 
-    try:
-        return date.strptime(date_string, std_date_format)
-    except:
-        return today
-
-def date_format(d: date) -> str:
-    '''String representation of date object in standard format'''
-    return d.strftime(std_date_format)
-
-def get_devs(devs: str) -> list[str]:
-    '''Get list of devs from the input string'''
-    devs = devs.strip()
-    if devs == '@goodapps':
-        devs = os.getenv('GOODAPPS_DEVS') or ''
-    if devs == '':
-        return []
-    return [x.strip() for x in devs.split(',')]
-
-def get_dev_limit() -> int:
-    '''Get limit for number of devs'''
-    try:
-        limit = int(os.getenv('DEV_LIMIT') or '9')
-        return max(1, limit) # floor dev limit = 1
-    except:
-        return 9 # default limit
+    data: Optional[T] = None 
+    message: str = OK_MESSAGE
 
 def get_cors_list() -> list[str]:
     '''Get list of origins allowed for CORS policy'''
---
+++ old/main.py
@@ -0,0 +1,66 @@
+from .data import  new_date, get_devs, get_dev_limit
+from .github import get_devs_contribs
+from .api import get_dev_repos, get_devs_repos, get_repo_languages, get_dev_languages
+
+
+@app.get('/contribs/{date_string}')
+async def get_month_data(date_string: str = 'today', devs: str = '', force: bool = False) -> DataResult:
+    input_date = new_date(date_string)
+    devs_list = get_devs(devs)
+    num_devs = len(devs_list)
+    dev_limit = get_dev_limit()
+    if num_devs == 0:
+        return DataResult(data=None, message='Empty devs list')
+    elif num_devs > dev_limit:
+        return DataResult(data=None, message=f'Devs list exceeds limit: {dev_limit}')
+    
+    dev_contribs, err = await get_devs_contribs(devs_list, input_date, force)
+    if err.has:
+        return DataResult(data=None, message=err.message)
+    
+    return DataResult(data = {
+        'date' : input_date,
+        'contribs': dev_contribs,
+    })
+
+@app.get('/devs/repos')
+async def get_devs_repos_data(devs: str = '', force: bool= False) -> DataResult:
+    devs_list = get_devs(devs)
+    num_devs = len(devs_list)
+    dev_limit = get_dev_limit()
+    if num_devs == 0:
+        return DataResult(data=None, message='Empty devs list')
+    elif num_devs > dev_limit:
+        return DataResult(data=None, message=f'Devs list exceeds limit: {dev_limit}')
+    
+    devs_repos, err = await get_devs_repos(devs_list, force)
+    if err.has:
+        return DataResult(data=None, message=err.message)
+    return DataResult(data = devs_repos)
+
+@app.get('/devs/list/{devs}')
+async def get_devs_data(devs: str) -> DataResult:
+    devs_list = get_devs(devs)
+    return DataResult(data = devs_list)
+
+
+@app.get('/repos/{dev}')
+async def get_dev_repos_data(dev: str, force: bool = False) -> DataResult:
+    dev_repos, err = await get_dev_repos(dev, force)
+    if err.has:
+        return DataResult(data=None, message=err.message)
+    return DataResult(data = dev_repos)
+
+@app.get('/repo/{dev}/{repo}/languages')
+async def get_repo_languages_data(dev: str, repo: str, force: bool=False) -> DataResult:
+    languages, err = await get_repo_languages(f'{dev}/{repo}', force)
+    if err.has:
+        return DataResult(data=None, message=err.message)
+    return DataResult(data = languages)
+    
+@app.get('/languages/{dev}')
+async def get_dev_languages_data(dev: str, force: bool = False) -> DataResult:
+    dev_languages, err = await get_dev_languages(dev, force)
+    if err.has:
+        return DataResult(data=None, message=err.message)
+    return DataResult(data = dev_languages)
